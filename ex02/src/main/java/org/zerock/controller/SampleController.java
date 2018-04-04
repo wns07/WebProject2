6090cb55ad4b60eacaa6c3126e7012bd6f8bbfb2
cho <cho@DESKTOP-C5C3H9L>
2018-04-04 14:46:55 +0900

    객체를 JSON으로 반환(p353~355)

--- ex02/src/main/java/org/zerock/controller/SampleController.java
+++ ex02/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.*;
 
 @RestController
 @RequestMapping("/sample")
@@ -9,6 +10,16 @@ public class SampleController {
 	@RequestMapping("/hello")
 	public String sayHello() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/sendVO")
+	public SampleVO sendVO() {
+		SampleVO vo = new SampleVO();
+		vo.setFirstName("길동");
+		vo.setLastName("홍");
+		vo.setMno(123);
+		
+		return vo;
 	}
 	
 }
