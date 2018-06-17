d544e494448625c5e93938b7a361dbeb0ab5e7a6
wns07 <[comeco0724@naver.com]>
2018-06-17 17:33:55 +0900

    Chapter02. 전송된 파일 저장(p546~558)

---
+++ ex04/src/main/java/org/zerock/util/UploadFileUtils.java
@@ -0,0 +1,78 @@
+package org.zerock.util;
+
+import java.awt.image.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+import javax.imageio.*;
+
+import org.imgscalr.*;
+import org.slf4j.*;
+import org.springframework.util.*;
+
+public class UploadFileUtils {
+	private static final Logger logger = LoggerFactory.getLogger(UploadFileUtils.class);
+	
+	private static String calcPath(String uploadPath) {
+		Calendar cal = Calendar.getInstance();
+		String yearPath = File.separator + cal.get(Calendar.YEAR);
+		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH) + 1);
+		String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
+		
+		makeDir(uploadPath, yearPath, monthPath, datePath);
+		
+		logger.info(datePath);
+		
+		return datePath;
+	}
+	
+	public static void makeDir(String uploadPath, String... paths) {
+		if(new File(paths[paths.length-1]).exists()) {
+			return;
+		}
+		
+		for(String path : paths) {
+			File dirPath = new File(uploadPath + path);
+			
+			if(!dirPath.exists()) {
+				dirPath.mkdir();
+			}
+		}
+	}
+	
+	private static String makeThumbnail(String uploadPath, String path, String fileName) throws Exception {
+		BufferedImage sourceImg = ImageIO.read(new File(uploadPath + path, fileName));
+		BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100);
+		String thumbnailName = uploadPath + path + File.separator + "s_" + fileName;
+		File newFile = new File(thumbnailName);
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		ImageIO.write(destImg, formatName.toUpperCase(), newFile);
+		
+		return thumbnailName.substring(uploadPath.length()).replace(File.separatorChar, '/');
+	}
+	
+	public static String uploadFile(String uploadPath, String originalName, byte[] fileData) throws Exception {
+		UUID uid = UUID.randomUUID();
+		String savedName = uid.toString() + "_" + originalName;
+		String savedPath = calcPath(uploadPath);
+		File target = new File(uploadPath + savedPath, savedName);
+		FileCopyUtils.copy(fileData, target);
+		String formatName = originalName.substring(originalName.lastIndexOf(".") + 1);
+		String uploadedFileName = null;
+		
+		if(MediaUtils.getMediaType(formatName) != null) {
+			uploadedFileName = makeThumbnail(uploadPath, savedPath, savedName);
+		} else {
+			uploadedFileName = makeIcon(uploadPath, savedPath, savedName);
+		}
+		
+		return uploadedFileName;
+	}
+	
+	private static String makeIcon(String uploadPath, String path, String fileName) throws Exception {
+		String iconName = uploadPath + path + File.separator + fileName;
+		
+		return iconName.substring(uploadPath.length()).replace(File.separatorChar, '/');
+	}
+}
