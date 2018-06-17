3837eee83f8d926a1cbb7d5b6858f652ca769825
wns07 <[comeco0724@naver.com]>
2018-06-17 15:25:06 +0900

    업로드 파일의 하드디스크 저장(p526~530)

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -1,14 +1,23 @@
 package org.zerock.controller;
 
+import java.io.*;
+import java.util.*;
+
+import javax.annotation.*;
+
 import org.slf4j.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
+import org.springframework.util.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.*;
 
 @Controller
 public class UploadController {
 	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@Resource(name="uploadPath")
+	private String uploadPath;
 	
 	@RequestMapping(value="/uploadForm", method=RequestMethod.GET)
 	public void uploadForm() {
@@ -20,5 +29,18 @@ public class UploadController {
 		logger.info("originalName : " + file.getOriginalFilename());
 		logger.info("size : " + file.getSize());
 		logger.info("contentType : " + file.getContentType());
+		
+		String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		
+		model.addAttribute("savedName", savedName);
 	}
+	
+	private String uploadFile(String originalName, byte[] fileData) throws Exception {
+		UUID uid = UUID.randomUUID();
+		String savedName = uid.toString() + "_" + originalName;
+		File target = new File(uploadPath, savedName);
+		FileCopyUtils.copy(fileData, target);
+		return savedName;
+	}
+	
 }
