a595ba5b9bb6ef40197e7c30181f6da3b2a5eb6a
wns07 <[comeco0724@naver.com]>
2018-06-17 15:48:53 +0900

    <iframe>을 이용한 파일업로드(p530~535)

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -25,7 +25,7 @@ public class UploadController {
 	}
 	
 	@RequestMapping(value="/uploadForm", method=RequestMethod.POST)
-	public void uploadForm(MultipartFile file, Model model) throws Exception {
+	public String uploadForm(MultipartFile file, Model model) throws Exception {
 		logger.info("originalName : " + file.getOriginalFilename());
 		logger.info("size : " + file.getSize());
 		logger.info("contentType : " + file.getContentType());
@@ -33,6 +33,8 @@ public class UploadController {
 		String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
 		
 		model.addAttribute("savedName", savedName);
+		
+		return "uploadResult";
 	}
 	
 	private String uploadFile(String originalName, byte[] fileData) throws Exception {
--- ex04/src/main/webapp/WEB-INF/views/uploadForm.jsp
+++ ex04/src/main/webapp/WEB-INF/views/uploadForm.jsp
@@ -4,11 +4,28 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Insert title here</title>
+	
+	<style>
+		iframe {
+			width : 0px;
+			height : 0px;
+			border : 0px;
+		}
+	</style>
 </head>
 <body>
-	<form id="form1" action="uploadForm" method="post" enctype="multipart/form-data">
+	<form id="form1" action="uploadForm" method="post" enctype="multipart/form-data" target="zeroFrame">
 		<input type="file" name="file">
 		<input type="submit">
 	</form>
+	
+	<iframe name="zeroFrame"></iframe>
+	
+	<script type="text/javascript">
+		function addFilePath(msg) {
+			alert(msg);
+			document.getElementById("form1").reset();
+		}
+	</script>
 </body>
 </html>
