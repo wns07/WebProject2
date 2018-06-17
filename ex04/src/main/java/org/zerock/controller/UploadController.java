82453aea25394d080f5d3174220f473afe0a9c69
wns07 <[comeco0724@naver.com]>
2018-06-17 15:06:41 +0900

    post방식의 파일업로드(p524~526)

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -2,7 +2,9 @@ package org.zerock.controller;
 
 import org.slf4j.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.*;
 
 @Controller
 public class UploadController {
@@ -11,5 +13,12 @@ public class UploadController {
 	@RequestMapping(value="/uploadForm", method=RequestMethod.GET)
 	public void uploadForm() {
 		
+	}
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.POST)
+	public void uploadForm(MultipartFile file, Model model) throws Exception {
+		logger.info("originalName : " + file.getOriginalFilename());
+		logger.info("size : " + file.getSize());
+		logger.info("contentType : " + file.getContentType());
 	}
 }
